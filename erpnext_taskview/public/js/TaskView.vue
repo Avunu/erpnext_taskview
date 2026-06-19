07d1f5871137e882a54a73e1a70348b14db2e8ad
Avunu LLC <mail@avu.nu>
2026-06-19 16:17:40 -0400

    fix: remove problematic icon class

--- erpnext_taskview/public/js/TaskView.vue
+++ erpnext_taskview/public/js/TaskView.vue
@@ -20,7 +20,7 @@
             @click="toggleNode(node, stat)"
             :class="{ open: stat.open }"
           >
-            <div class="icon-container">
+            <div class="taskview-tree-icon">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <title>chevron-right</title>
                 <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
